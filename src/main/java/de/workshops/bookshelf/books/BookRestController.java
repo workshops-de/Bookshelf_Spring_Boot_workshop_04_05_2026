df68fbc53ae9e78821d0827378ac65a265710e65
Birgit Kratz <mail@birgitkratz.de>
2026-05-04 20:30:21 +0200

    Day 1: Introduction, setup application, first controller method

---
+++ src/main/java/de/workshops/bookshelf/books/BookRestController.java
@@ -0,0 +1,37 @@
+package de.workshops.bookshelf.books;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookRestController {
+    private final ObjectMapper mapper;
+    private final ResourceLoader resourceLoader;
+
+    private List<Book> books;
+
+    public BookRestController(ObjectMapper mapper, ResourceLoader resourceLoader) {
+        this.mapper = mapper;
+        this.resourceLoader = resourceLoader;
+    }
+
+    @PostConstruct
+    public void init() throws IOException {
+        final var resource = resourceLoader.getResource("classpath:books.json");
+        this.books = mapper.readValue(resource.getInputStream(), new TypeReference<>() {});
+    }
+
+    @GetMapping
+    List<Book> getAllBooks() {
+        return books;
+    }
+}
